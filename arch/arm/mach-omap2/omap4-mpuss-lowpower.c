c78adb7fb9a6de9ec661aa79f6d070dafb0ce84b
Carlos Leija <cileija@ti.com>
2011-03-11 02:12:46 -0600

    OMAP4: PM: Add call to omap4_secure_dispatcher early in CPU0 wake up
    This will restore the kernel return address for HAL calls.
    
    Signed-off-by: Carlos Leija <cileija@ti.com>

--- arch/arm/mach-omap2/omap4-mpuss-lowpower.c
+++ arch/arm/mach-omap2/omap4-mpuss-lowpower.c
@@ -577,6 +577,7 @@ cpu_prepare:
 
 	if ((omap4_device_off_read_prev_state()) &&
 			(omap_type() != OMAP2_DEVICE_TYPE_GP)) {
+		omap4_secure_dispatcher(0x21, 4, 0, 0, 0, 0, 0);
 		restore_ivahd_tesla_regs();
 		restore_l3instr_regs();
 	}
