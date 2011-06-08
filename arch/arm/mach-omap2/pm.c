2c19445db400f7e2dcd375c92882636e6320f86d
Nishanth Menon <nm@ti.com>
2011-06-08 10:01:10 -0500

    OMAP2+: PM: PMIC: rename of pmic_init
    To sync back on 3.0-rc2 upstream code, we choose to rename our non-upstream
    version to omap_pmic_data_init
    
    Signed-off-by: Nishanth Menon <nm@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -268,7 +268,7 @@ postcore_initcall(omap2_common_pm_init);
 static int __init omap2_common_pm_late_init(void)
 {
 	/* Init the OMAP PMIC parameters */
-	omap_pmic_init();
+	omap_pmic_data_init();
 
 	/* Init the voltage layer */
 	omap_voltage_late_init();
