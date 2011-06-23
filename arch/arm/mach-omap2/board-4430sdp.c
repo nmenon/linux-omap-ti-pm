KFire-Android/kernel_omap_otter-common

omap4: Reconfigure the DDR timings on SDP and Panda
This patch reconfigures DDR timings to OPP100. The 4430 SDP
and Panda boards uses ELPIDA lpddr2 memories.

The memory organisation is as below :
	EMIF1 - CS0 -	2 Gb
		CS1 -	2 Gb
	EMIF2 - CS0 -	2 Gb
		CS1 -	2 Gb
	--------------------
	TOTAL -		8 Gb

Signed-off-by: Santosh Shilimkar <santosh.shilimkar@ti.com>
Signed-off-by: Aneesh V <aneesh@ti.com>

arch/arm/mach-omap2/board-4430sdp.c
@@ -33,6 +33,9 @@
 
 #include <mach/hardware.h>
 #include <mach/omap4-common.h>
+#include <mach/emif.h>
+#include <mach/lpddr2-elpida.h>
+
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -1029,6 +1032,23 @@ static struct omap_i2c_bus_board_data __initdata sdp4430_i2c_2_bus_pdata;
 static struct omap_i2c_bus_board_data __initdata sdp4430_i2c_3_bus_pdata;
 static struct omap_i2c_bus_board_data __initdata sdp4430_i2c_4_bus_pdata;
 
+/*
+ * LPDDR2 Configeration Data:
+ * The memory organisation is as below :
+ *	EMIF1 - CS0 -	2 Gb
+ *		CS1 -	2 Gb
+ *	EMIF2 - CS0 -	2 Gb
+ *		CS1 -	2 Gb
+ *	--------------------
+ *	TOTAL -		8 Gb
+ *
+ * Same devices installed on EMIF1 and EMIF2
+ */
+static __initdata struct emif_device_details emif_devices = {
+	.cs0_device = &elpida_2G_S4,
+	.cs1_device = &elpida_2G_S4
+};
+
 static void __init omap_i2c_hwspinlock_init(int bus_id, unsigned int
 			spinlock_id, struct omap_i2c_bus_board_data *pdata)
 {
@@ -1203,6 +1223,8 @@ static void __init omap_4430sdp_init(void)
 {
 	int status;
 
+	omap_emif_setup_device_details(&emif_devices, &emif_devices);
+	omap_init_emif_timings();
 	omap4_i2c_init();
 	omap4_display_init();
 	omap_disp_led_init();

nmenon/linux-omap-ti-pm

arch/arm/mach-omap2/board-4430sdp.c
@@ -27,6 +27,9 @@
 
 #include <mach/hardware.h>
 #include <mach/omap4-common.h>
+#include <mach/emif.h>
+#include <mach/lpddr2-elpida.h>
+
 #include <asm/mach-types.h>
 #include <asm/mach/arch.h>
 #include <asm/mach/map.h>
@@ -288,6 +291,7 @@ static struct platform_device *sdp4430_devices[] __initdata = {
 
 static struct omap_board_config_kernel sdp4430_config[] __initdata = {
 };
+
 
 static void __init omap_4430sdp_init_early(void)
 {
@@ -823,6 +827,23 @@ static struct omap_board_data serial4_data __initdata = {
 	.pads_cnt	= ARRAY_SIZE(serial4_pads),
 };
 
+/*
+ * LPDDR2 Configeration Data:
+ * The memory organisation is as below :
+ *	EMIF1 - CS0 -	2 Gb
+ *		CS1 -	2 Gb
+ *	EMIF2 - CS0 -	2 Gb
+ *		CS1 -	2 Gb
+ *	--------------------
+ *	TOTAL -		8 Gb
+ *
+ * Same devices installed on EMIF1 and EMIF2
+ */
+static __initdata struct emif_device_details emif_devices = {
+	.cs0_device = &lpddr2_elpida_2G_S4_dev,
+	.cs1_device = &lpddr2_elpida_2G_S4_dev
+};
+
 static inline void board_serial_init(void)
 {
 	struct omap_board_data bdata;
@@ -854,6 +875,8 @@ static void __init omap_4430sdp_init(void)
 	if (omap_rev() == OMAP4430_REV_ES1_0)
 		package = OMAP_PACKAGE_CBL;
 	omap4_mux_init(board_mux, NULL, package);
+
+	omap_emif_setup_device_details(&emif_devices, &emif_devices);
 
 	omap_board_config = sdp4430_config;
 	omap_board_config_size = ARRAY_SIZE(sdp4430_config);