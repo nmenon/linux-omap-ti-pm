62208b82c6806b5d57add1e695d374327cb479c3
Nishanth Menon <nm@ti.com>
2011-09-17 04:41:46 -0500

    OMAP: add custom tracepoint
    Add custom tracepoint for OMAP devices
    
    Change-Id: I65e790499c9b39bfa71cbc16a58edde5f36db26d
    Signed-off-by: Nishanth Menon <nm@ti.com>

---
+++ arch/arm/mach-omap2/traces.c
@@ -0,0 +1,15 @@
+/*
+ * Power trace points
+ *
+ * Copyright (C) 2009 Arjan van de Ven <arjan@linux.intel.com>
+ */
+
+#include <linux/string.h>
+#include <linux/types.h>
+#include <linux/workqueue.h>
+#include <linux/sched.h>
+#include <linux/module.h>
+
+#define CREATE_TRACE_POINTS
+#include <trace/events/omap.h>
+
---
+++ include/trace/events/omap.h
@@ -0,0 +1,63 @@
+
+#undef TRACE_SYSTEM
+#define TRACE_SYSTEM omap
+
+#if !defined(_TRACE_OMAP_H) || defined(TRACE_HEADER_MULTI_READ)
+#define _TRACE_OMAP_H
+
+#include <linux/ktime.h>
+#include <linux/tracepoint.h>
+
+/* The OMAP events with flags */
+DECLARE_EVENT_CLASS(omapflag,
+
+	TP_PROTO(const char *name, unsigned int state, unsigned int flags, unsigned int cpu_id),
+
+	TP_ARGS(name, state, flags, cpu_id),
+
+	TP_STRUCT__entry(
+		__string(       name,           name            )
+		__field(        u64,            state           )
+		__field(        u32,            flags           )
+		__field(        u64,            cpu_id          )
+	),
+
+	TP_fast_assign(
+		__assign_str(name, name);
+		__entry->state = state;
+		__entry->flags = flags;
+		__entry->cpu_id = cpu_id;
+	),
+
+	TP_printk("%s state=%lu flags = %lu, cpu_id=%lu", __get_str(name),
+		(unsigned long)__entry->state, (unsigned long)__entry->flags, (unsigned long)__entry->cpu_id)
+);
+
+/* The regular OMAP events */
+DECLARE_EVENT_CLASS(omap,
+
+	TP_PROTO(const char *name, unsigned int state, unsigned int cpu_id),
+
+	TP_ARGS(name, state, cpu_id),
+
+	TP_STRUCT__entry(
+		__string(       name,           name            )
+		__field(        u64,            state           )
+		__field(        u64,            cpu_id          )
+	),
+
+	TP_fast_assign(
+		__assign_str(name, name);
+		__entry->state = state;
+		__entry->cpu_id = cpu_id;
+	),
+
+	TP_printk("%s state=%lu cpu_id=%lu", __get_str(name),
+		(unsigned long)__entry->state, (unsigned long)__entry->cpu_id)
+);
+
+
+#endif /* _TRACE_OMAP_H */
+
+#include <trace/define_trace.h>
+
